0f36315e713017101c984dc8e9383ef07499ed36
Xiangxu Lin <cricel.design@gmail.com>
2025-10-14 17:50:50 -0400

    init testing files

---
+++ main.py
@@ -0,0 +1,16 @@
+# main.py
+
+# Option 1: Import the entire module
+import math_utils
+
+result_sum = math_utils.add(5, 3)
+result_product = math_utils.multiply(5, 3)
+
+print("Sum:", result_sum)
+print("Product:", result_product)
+
+
+# Option 2 (alternative): Import specific functions
+# from math_utils import add, multiply
+# print("Sum:", add(10, 4))
+# print("Product:", multiply(10, 4))
---
+++ math_utils.py
@@ -0,0 +1,9 @@
+# math_utils.py
+
+def add(a, b):
+    """Return the sum of two numbers."""
+    return a + b
+
+def multiply(a, b):
+    """Return the product of two numbers."""
+    return a * b
